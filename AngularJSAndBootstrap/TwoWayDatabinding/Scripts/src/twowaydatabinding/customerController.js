6c24ac952f1335c4dd52277868db3715ce7af338
IPGNA\predrag.ruzic <predrag.ruzic@interpublic.com>
2016-12-14 19:35:15 -0600

    AngularJS two way data binding

---
+++ AngularJSAndBootstrap/TwoWayDatabinding/Scripts/src/twowaydatabinding/customerController.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('app')
+        .controller('customerController', CustomerController);
+
+    function CustomerController() {
+
+        var cst = this;
+        console.log("From Controller");
+        cst.customer = {
+            firstName: 'Predrag',
+            lastName: 'Ruzic',
+            gender: 'M'
+        };
+
+
+
+        console.log("Customer: " + cst.customer.firstName);
+    }
+
+})();
